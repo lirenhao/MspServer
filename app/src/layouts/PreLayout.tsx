657ec0b04c7cb65a5234007af931ead264038eb6
lirenhao <renhao.li@bjyada.com>
2020-08-20 16:29:20 +0800

    刷新状态判断

--- app/src/layouts/PreLayout.tsx
+++ app/src/layouts/PreLayout.tsx
@@ -1,33 +1,53 @@
 import React from 'react';
 import { Dispatch } from 'redux';
 import { connect } from 'dva';
+import { Redirect } from 'umi';
+import { PageLoading } from '@ant-design/pro-layout';
 import ProLayout, { BasicLayoutProps as ProLayoutProps, Settings } from '@ant-design/pro-layout';
 import { ConnectState } from '@/models/connect';
 import RightContent from '@/components/GlobalHeader/RightContent';
+import { User } from '@/models/user';
 import BasicFooter from './BasicFooter';
 import logo from '../assets/logo.png';
 
 export interface LayoutProps extends ProLayoutProps {
   settings: Settings;
   dispatch: Dispatch<any>;
+  loading: boolean | undefined;
+  user: User;
 }
 
 const PreLayout: React.FC<LayoutProps> = props => {
-  const { settings, children } = props;
+  const { dispatch, settings, loading, user, children } = props;
 
-  return (
-    <ProLayout
-      logo={logo}
-      footerRender={() => <BasicFooter />}
-      rightContentRender={() => <RightContent />}
-      {...settings}
-    >
-      {children}
-    </ProLayout>
-  );
+  React.useEffect(() => {
+    dispatch({ type: 'user/fetchUser' });
+  }, []);
+
+  if (!loading && user) {
+    switch (user.status) {
+      case "00": {
+        return <Redirect to="/"></Redirect>;
+      }
+      default: {
+        return (
+          <ProLayout
+            logo={logo}
+            footerRender={() => <BasicFooter />}
+            rightContentRender={() => <RightContent />}
+            {...settings}
+          >
+            {children}
+          </ProLayout>);
+      }
+    }
+  } else {
+    return <PageLoading />;
+  }
 };
 
-export default connect(({ global, settings }: ConnectState) => ({
-  collapsed: global.collapsed,
+export default connect(({ settings, user, loading }: ConnectState) => ({
   settings,
+  user: user.user,
+  loading: loading.models.user,
 }))(PreLayout);
