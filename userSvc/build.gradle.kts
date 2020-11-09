bc31c06b8714bd4578f7b0ff8de1f61c214613e6
lirenhao <renhao.li@bjyada.com>
2020-11-09 11:26:36 +0800

    依赖版本升级

--- userSvc/build.gradle.kts
+++ userSvc/build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.3.1.RELEASE"
-    id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    id("org.springframework.boot") version "2.3.5.RELEASE"
+    id("io.spring.dependency-management") version "1.0.10.RELEASE"
     kotlin("jvm") version "1.3.72"
     kotlin("plugin.spring") version "1.3.72"
 }
